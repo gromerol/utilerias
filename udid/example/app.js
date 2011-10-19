ed84065af56624036e195c194a3266307879ae68
Matt Apperson <mapperson@appcelerator.com>
2011-10-19 15:05:44 -0400

    Added UDID module

---
+++ udid/example/app.js
@@ -0,0 +1,19 @@
+// This is a test harness for your module
+// You should do something interesting in this harness 
+// to test out the module and to provide instructions 
+// to users on how to use it by example.
+
+
+// open a single window
+var window = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+var label = Ti.UI.createLabel();
+window.add(label);
+window.open();
+
+// TODO: write your module tests here
+var udid = require('ti.udid');
+Ti.API.info("module is => " + udid);
+
+label.text = udid.udid;
