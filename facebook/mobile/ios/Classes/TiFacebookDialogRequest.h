629dedd2b0bd8d82082753af3bce65b119da7eb2
Blain Hamon <blainhamon@mac.com>
2012-11-01 10:32:54 -0700

    TIMOB-10422 First pass at facebook module

---
+++ facebook/mobile/ios/Classes/TiFacebookDialogRequest.h
@@ -0,0 +1,19 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#import "FBConnect/Facebook.h"
+#import "FacebookModule.h"
+#import "KrollCallback.h"
+
+@interface TiFacebookDialogRequest : NSObject <FBDialogDelegate> {
+
+	KrollCallback *callback;
+	FacebookModule *module;
+}
+
+-(id)initWithCallback:(KrollCallback*)callback_ module:(FacebookModule*)module_;
+
+@end
