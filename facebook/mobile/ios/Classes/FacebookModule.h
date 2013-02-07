68eb67bfff274363b62431a1f9bba7d28d1e9f5a
jonalter <jonalter+github@gmail.com>
2013-02-07 10:37:06 -0800

    [MOD-1270] Facebook: adding attribution and updating copyright in source

--- facebook/mobile/ios/Classes/FacebookModule.h
+++ facebook/mobile/ios/Classes/FacebookModule.h
@@ -1,9 +1,9 @@
 /**
- * Appcelerator Titanium Mobile
+ * Facebook Module
  * Copyright (c) 2009-2013 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+
 #import "TiModule.h"
 #import "Facebook.h"
 
