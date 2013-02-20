8834ecb2698b95aa4ee2aaec528719b8e83015d7
Ping Wang <pwang@appcelerator.com>
2013-02-19 17:15:17 -0800

    timob-12529: address comments

--- facebook/mobile/android/src/facebook/SessionEvents.java
+++ facebook/mobile/android/src/facebook/SessionEvents.java
@@ -16,10 +16,8 @@
 
 /**
  * MODIFICATIONS
- * 
- * Appcelerator Titanium Mobile
- * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
+ * Facebook Module
+ * Copyright (c) 2009-2013 by Appcelerator, Inc. All Rights Reserved.
  * Please see the LICENSE included with this distribution for details.
  */
 
